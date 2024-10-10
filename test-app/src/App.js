0e862c0d1781adc471b7f2e0e1d3b062e9c816ae
msnkr <mikyle.designs@gmail.com>
2024-10-10 09:45:47 +0200

    .

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+const wording = "Congrats"
 
-function Profile() {
+function Congrats() {
   return (
-    <img className=" rounded-full w-[100px]"
-      src="https://i.imgur.com/MK3eW3Am.jpg"
-      alt="Katherine Johnson"
-    />
+    <div>
+      <h1 className="text-4xl text-gray-400 font-bold p-8">
+        {wording}
+      </h1>
+    </div>
   )
 }
 
-export default function Gallery() {
+export default function CongratsPage() {
   return (
     <div>
-      <h1 className="text-4xl text-gray-400">Amazing Scientist</h1>
-      <div className="flex space-x-8 p-8">
-        <Profile />
-        <Profile />
-        <Profile />
-      </div>
+      <Congrats />
     </div>
   )
 }
