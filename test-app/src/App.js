a50d42a428bd325f96367758476318a6273155fb
msnkr <mikyle.designs@gmail.com>
2024-10-10 12:39:13 +0200

    .

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,10 +1,31 @@
-import Gallery from "./Gallery";
 
+const baseUrl = 'https://i.imgur.com/';
+const person = {
+  name: 'Gregorio Y. Zara',
+  imageId: '7vQD0fP',
+  imageSize: 'b',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
 
-export default function App() {
+const url = baseUrl + person.imageId + person.imageSize + ".jpg"
+
+export default function TodoList() {
   return (
-    <div>
-      <Gallery />
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src={url}
+        alt={person.name}
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
     </div>
-  )
+  );
 }
