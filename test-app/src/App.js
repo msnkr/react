efa63584c6bfd733c3c745537fc7d855c6a021f6
msnkr <mikyle.designs@gmail.com>
2024-10-10 09:59:47 +0200

    .

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,22 +1,2 @@
-import logo from './logo.svg';
-import './App.css';
-
-const wording = "Congrats"
-
-function Congrats() {
-  return (
-    <div>
-      <h1 className="text-4xl text-gray-400 font-bold p-8">
-        {wording}
-      </h1>
-    </div>
-  )
-}
-
-export default function CongratsPage() {
-  return (
-    <div>
-      <Congrats />
-    </div>
-  )
-}
+// import logo from './logo.svg';
+// import './App.css';
