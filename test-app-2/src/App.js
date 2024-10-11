44977383e71834443afcdba1428085fab4b4856d
msnkr <mikyle.designs@gmail.com>
2024-10-11 23:00:53 +0200

    .

--- test-app-2/src/App.js
+++ test-app-2/src/App.js
@@ -1,7 +1,14 @@
+const fruits = ["apple", "grapes", "mango"];
+
+const listItems = fruits.map((fruit) => (
+  <li className="text-gray-400">{fruit}</li>
+));
+
 export default function Hello() {
   return (
     <div className="container mt-[100px] ml-[100px]">
       <h1 className="text-4xl font-bold text-red-400">Hello, World!</h1>
+      <ul className="space-y-2 mt-6">{listItems}</ul>
     </div>
   );
 }
