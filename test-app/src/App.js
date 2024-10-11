71dac8a4cdfb7be9ea6873db982eca2a1a4be038
msnkr <mikyle.designs@gmail.com>
2024-10-11 13:21:00 +0200

    .

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,24 +1,6 @@
-function Drink({ name }) {
-  return (
-    <section>
-      <h1>{name}</h1>
-      <dl>
-        <dt>Part of plant</dt>
-        <dd>{name === 'tea' ? 'leaf' : 'bean'}</dd>
-        <dt>Caffeine content</dt>
-        <dd>{name === 'tea' ? '15–70 mg/cup' : '80–185 mg/cup'}</dd>
-        <dt>Age</dt>
-        <dd>{name === 'tea' ? '4,000+ years' : '1,000+ years'}</dd>
-      </dl>
-    </section>
-  );
-}
+import {UserData} from "./data.js"
 
-export default function DrinkList() {
-  return (
-    <div>
-      <Drink name="tea" />
-      <Drink name="coffee" />
-    </div>
-  );
+
+export default function ShowData() {
+  return console.log(UserData)
 }
