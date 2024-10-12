97f71d3f3f5be6992319dbd2b93cd4c5da10d25e
msnkr <mikyle.designs@gmail.com>
2024-10-12 22:38:55 +0200

    .

--- keeper-app/src/App.js
+++ keeper-app/src/App.js
@@ -1,11 +1,13 @@
 import "./style.css";
 import { Heading } from "./components/header";
 import { Footer } from "./components/footer";
+import { Notes } from "./components/note";
 
 export default function App() {
   return (
     <div>
       <Heading />
+      <Notes />
       <Footer />
     </div>
   );
