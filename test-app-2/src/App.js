a324b21d5382951859877141e9941d376da75d3a
msnkr <mikyle.designs@gmail.com>
2024-10-12 19:06:35 +0200

    .

--- test-app-2/src/App.js
+++ test-app-2/src/App.js
@@ -46,22 +46,7 @@
 //   );
 // }
 
-const heading = {
-  fontSize: "50px",
-  fontWeight: "bold",
-  borderBottom: "5px solid black",
-};
-
-function ShowDate() {
-  let hours = new Date().getHours();
-  if (hours > 0 && hours < 12) {
-    return <h1 style={{ ...heading, color: "red" }}>Good Morning</h1>;
-  } else if (hours > 12 && hours < 18) {
-    return <h1 style={{ ...heading, color: "green" }}>Good Afternoon</h1>;
-  } else {
-    return <h1 style={{ ...heading, color: "blue" }}>Good Evening</h1>;
-  }
-}
+import { ShowDate } from "./ShowDate";
 
 export default function App() {
   return (
