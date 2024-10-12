bcf7a8eedb5e6a538e6d97a0b1a71120876c0d2a
msnkr <mikyle.designs@gmail.com>
2024-10-12 17:28:10 +0200

    .

--- test-app-2/src/App.js
+++ test-app-2/src/App.js
@@ -13,21 +13,35 @@
 //   );
 // }
 
-export default function Images() {
+// export default function Images() {
+//   return (
+//     <div className="flex space-x-8 justify-center mt-20">
+//       <img
+//         className="w-[200px] rounded-xl"
+//         src="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//       />
+//       <img
+//         className="w-[200px] rounded-xl"
+//         src="https://images.pexels.com/photos/115655/pexels-photo-115655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//       />
+//       <img
+//         className="w-[200px] rounded-xl"
+//         src="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+//       />
+//     </div>
+//   );
+// }
+
+let year = new Date();
+const name = "msn";
+
+export default function App() {
   return (
-    <div className="flex space-x-8 justify-center mt-20">
-      <img
-        className="w-[200px] rounded-xl"
-        src="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
-      <img
-        className="w-[200px] rounded-xl"
-        src="https://images.pexels.com/photos/115655/pexels-photo-115655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
-      <img
-        className="w-[200px] rounded-xl"
-        src="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
+    <div className="mt-20 ml-20">
+      <h1 className="text-2xl font-semibold">
+        Created by <span className="text-red-300">{name}</span>
+      </h1>
+      <p className="font-xs text-gray-400">Copyright: {year.getFullYear()}</p>
     </div>
   );
 }
