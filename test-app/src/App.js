12a979313f7a6773ac7e756a4b6654982c1565a6
msnkr <mikyle.designs@gmail.com>
2024-10-14 11:16:04 +0200

    .

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,26 +1,65 @@
-import { people } from "./faker";
+// import { people } from "./faker";
 
-function Person({ people }) {
-  return (
-    <div className="grid grid-cols-4 gap-20 text-center">
-      {people.map((p, index) => (
-        <div key={index}>
-          <img className="rounded-xl" src={p.avatar} />
-          <h1 className="text-md font-semibold">{p.displayName}</h1>
-          <p className="text-xs text-gray-400">
-            {p.firstName} {p.lastName}
-          </p>
-          <p className="text-xs text-gray-400">{p.email}}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
+// function Person({ people }) {
+//   return (
+//     <div className="grid grid-cols-4 gap-20 text-center">
+//       {people.map((p, index) => (
+//         <div key={index}>
+//           <img className="rounded-xl" src={p.avatar} alt="None" />
+//           <h1 className="text-md font-semibold">{p.displayName}</h1>
+//           <p className="text-xs text-gray-400">
+//             {p.firstName} {p.lastName}
+//           </p>
+//           <p className="text-xs text-gray-400">{p.email}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <div className="p-8">
+//       <Person people={people} />
+//     </div>
+//   );
+// }
+
+// import { people } from "./faker";
+
+// function Person({ people }) {
+//   return (
+//     <div className="grid md:grid-cols-4 gap-12 text-center">
+//       {people.map((p, index) => (
+//         <div key={index} className="border p-8 rounded-xl space-y-1">
+//           <img className="rounded-xl shadow-lg" src={p.avatar} alt="Profile Image" />
+//           <h1>{p.displayName}</h1>
+//           <p className="text-xs text-gray-400">
+//             {p.firstName} {p.lastName}
+//           </p>
+//           <p>{p.email}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <div className="p-8">
+//       <div>
+//         <h1 className="text-4xl font-semibold text-center">Contacts</h1>
+//         <Person people={people} />
+//       </div>
+//     </div>
+//   );
+// }
+
 
 export default function App() {
-  return (
-    <div className="p-8">
-      <Person people={people} />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    )
 }
