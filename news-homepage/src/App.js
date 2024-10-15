78e18b189e70f50d905334274b0c3bed92f8bb2b
msnkr <mikyle.designs@gmail.com>
2024-10-15 21:08:58 +0200

    .

--- news-homepage/src/App.js
+++ news-homepage/src/App.js
@@ -1,9 +1,29 @@
 // https://www.frontendmentor.io/challenges/news-homepage-H6SWTa1MFl
 
+
+import Navbar from "./components/navbar";
+import HeroImage from "./images/image-web-3-mobile.jpg";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl text-gray-400 bg-black p-8">Hello, World!</h1>
+    <div className="font-customFont">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="hero-section">
+        <div className="img-container px-4">
+          <img src={HeroImage} />
+        </div>
+        <div className="hero-heading px-8 py-2">
+          <h1 className="text-5xl w-[80%] font-bold capitalize">The bright future of web 3.0?</h1>
+        </div>
+        <div className="hero-wording px-8 py-2 text-lg text-gray-400">
+          <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
+        </div>
+        <div className="hero-button px-8 py-2">
+          <button className="uppercase font-lg bg-orange-600 px-8 py-2 font-bold">Read More</button>
+        </div>
+      </div>
     </div>
   );
 }
