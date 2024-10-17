59ecd3e9dc02b0ca9ad8fdb26972cd6d5d214486
msnkr <mikyle.designs@gmail.com>
2024-10-17 13:33:24 +0200

    .

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,15 +1,22 @@
 // https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW
 
 import backgroundImage from "./images/bg-mobile-dark.jpg";
+import LogoComponent from "./components/LogoComponent";
+import CreateTask from "./components/CreateTask";
 
 function App() {
   return (
-    <div className="font-customFont">
+    <div>
       <div className="main-container">
         <div className="background-container">
           <img className="w-full" src={backgroundImage} alt="home" />
         </div>
-        {/* <LogoComponent /> */}
+        <div className="logo-container">
+          <LogoComponent />
+        </div>
+        <div className="create-task-container">
+          <CreateTask />
+        </div>
       </div>
     </div>
   );
---
+++ calculator-app/src/components/CreateTask.js
@@ -0,0 +1,9 @@
+function CreateTask() {
+  return (
+    <div className="create-task-container text-center">
+      <input type="text" className="w-1/2 h-[40px] rounded-xl" />
+    </div>
+  );
+}
+
+export default CreateTask;
