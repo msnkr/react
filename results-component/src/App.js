b830cd75b635985387b66ff7a2c783b3fa31cb1b
msnkr <mikyle.designs@gmail.com>
2024-10-21 10:28:51 +0200

    .

--- results-component/src/App.js
+++ results-component/src/App.js
@@ -3,14 +3,14 @@ function App() {
     <div>
       <div className="main font-customFont">
         <div className="section-1">
-          <div className="results-section text-center bg-gradient-to-t from-light-royal-blue-background to-light-slate-blue-background px-12 py-4 text-white">
+          <div className="results-section text-center bg-gradient-to-t from-light-royal-blue-background to-light-slate-blue-background px-12 py-4 text-white rounded-b-[50px]">
             <p>Your Result</p>
-            <div className="score bg-light-royal-blue-background rounded-full w-[150px] h-[150px] flex flex-col justify-center mx-auto my-4 ">
+            <div className="score bg-gradient-to-t from-light-slate-blue-background to-light-royal-blue-background rounded-full w-[150px] h-[150px] flex flex-col justify-center mx-auto my-4 ">
               <h2 className="text-6xl font-semibold mb-2">76</h2>
               <p className="text-md">of 100</p>
             </div>
             <h2 className="my-2 text-3xl">Great</h2>
-            <p>
+            <p className="w-[300px] mx-auto">
               You scored higher than 65% of the people who have taken these
               tests.
             </p>
