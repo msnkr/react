4badc857f63d3ef56fc8ba305ff5032cae9e28ca
msnkr <mikyle.designs@gmail.com>
2024-10-21 09:52:34 +0200

    .

--- results-component/src/App.js
+++ results-component/src/App.js
@@ -1,9 +1,23 @@
 function App() {
   return (
     <div>
-      <h1 className="text-5xl font-bold text-gray-500 font-customFont">
-        Hello, World!
-      </h1>
+      <div className="main font-customFont">
+        <div className="section-1">
+          <div className="results-section text-center bg-gradient-to-t from-light-royal-blue-background to-light-slate-blue-background px-8 py-4 text-white">
+            <p>Your Result</p>
+            <div className="score">
+              <h2>76</h2>
+              <p>of 100</p>
+            </div>
+            <h2>Great</h2>
+            <p>
+              You scored higher than 65% of the people who have taken these
+              tests.
+            </p>
+          </div>
+        </div>
+        <div className="section-2"></div>
+      </div>
     </div>
   );
 }
