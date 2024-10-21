301ed76c7521b866ffd076d7dcc336f98704ea17
msnkr <mikyle.designs@gmail.com>
2024-10-21 08:19:20 +0200

    .

--- test-app-2/src/App.js
+++ test-app-2/src/App.js
@@ -133,3 +133,13 @@
 //     </div>
 //   );
 // }
+
+function SayHello() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-gray-500">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default SayHello;
