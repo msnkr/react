ab6a716b7722b3ec6aad59997961599958ea3e6e
msnkr <mikyle.designs@gmail.com>
2024-10-21 08:49:22 +0200

    .

---
+++ results-component/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-gray-500 font-customFont">
+        Hello, World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ results-component/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-red": "hsl(0, 100%, 67%)",
+        "orangey-yellow": "hsl(39, 100%, 56%)",
+        "green-teal": "hsl(166, 100%, 37%)",
+        "cobalt-blue": "hsl(234, 85%, 45%)",
+        "light-slate-blue-background": "hsl(252, 100%, 67%)",
+        "light-royal-blue-background": "hsl(241, 81%, 54%)",
+        "violet-blue-circle": "hsla(256, 72%, 46%, 1)",
+        "persian-blue-circle": "hsla(241, 72%, 46%, 0)",
+        "pale-blue": "hsl(221, 100%, 96%)",
+        "light-lavender": "hsl(241, 100%, 89%)",
+        "dark-gray blue": "hsl(224, 30%, 27%)",
+      },
+      fontFamily: {
+        customFont: "Hanken Grotesk",
+      },
+    },
+  },
+  plugins: [],
+};
