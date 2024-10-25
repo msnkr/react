30614710253d93e29b1e85dec638f49c49be9099
msnkr <mikyle.designs@gmail.com>
2024-10-25 11:08:48 +0200

    .

--- product-list-with-cart/src/App.js
+++ product-list-with-cart/src/App.js
@@ -1,3 +1,20 @@
+import Card from "./components/Card";
+import data from "./data.json";
+
+import waffle from "./images/image-waffle-mobile.jpg";
+
 export default function App() {
-  return <h1 className="">Hello world!</h1>;
+  return (
+    <div>
+      <div className="p-8">
+        <h1 className="text-5xl font-bold mb-8">Desserts</h1>
+        <Card
+          img={waffle}
+          subheading={data[0].category}
+          heading={data[0].name}
+          price={data[0].price}
+        />
+      </div>
+    </div>
+  );
 }
