84eff571887232e663393e65aaeaaa81f4ce47e0
msnkr <mikyle.designs@gmail.com>
2024-10-29 12:20:36 +0200

    .

--- product-list-with-cart/src/components/YourCart.js
+++ product-list-with-cart/src/components/YourCart.js
@@ -1,7 +1,15 @@
 function YourCart() {
   return (
     <div>
-      <div>Hello, World!</div>
+      <div className="mt-16 px-8">
+        <div>
+          <div className="bg-white">
+            <h1 className="text-4xl pt-4 font-bold text-rose-700">
+              Your Cart (8)
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
