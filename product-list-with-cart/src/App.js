317e284642a581f161d83429daf3e597aaceb1c0
msnkr <mikyle.designs@gmail.com>
2024-10-29 09:07:04 +0200

    .

--- product-list-with-cart/src/App.js
+++ product-list-with-cart/src/App.js
@@ -1,24 +1,9 @@
-import Card from "./components/Card";
 import dataFile from "./data.json";
 
 export default function App() {
   return (
     <div>
-      <div className="p-8">
-        <h1 className="text-5xl font-bold mb-8">Desserts</h1>
-        <div className="space-y-8">
-          {dataFile.map((data, index) => (
-            <div key={index}>
-              <Card
-                img={require(`${data.image.mobile}`)}
-                subheading={data.category}
-                heading={data.name}
-                price={data.price}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
+      <h1>Hello</h1>
     </div>
   );
 }
