07c4893f48ec66cc35dca91be30301cc0e9b1372
msnkr <mikyle.designs@gmail.com>
2024-10-29 23:13:38 +0200

    .

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -643,10 +643,34 @@
 
 function App() {
   return (
-    <div className="overflow-hidden">
-      <h1 className="text-2xl font-semibold text-center animate-translateX py-20 z-50">
-        Hello, World!
-      </h1>
+    <div className="overflow-hidden x">
+      <div>
+        <h1 className="text-2xl font-semibold text-center animate-translateX py-20 z-50 h-screen">
+          Hello, World!
+        </h1>
+      </div>
+      <div>
+        <p className="p-8 h-screen x">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div>
+        <p className="p-8 h-screen x animate-translateX">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
     </div>
   );
 }
