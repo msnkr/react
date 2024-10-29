e9162f0f99ec5f46cfec7d1dd89ca05efdf22909
msnkr <mikyle.designs@gmail.com>
2024-10-29 08:50:36 +0200

    .

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -640,39 +640,3 @@
 // }
 
 // export default App;
-
-import { useState } from "react";
-
-const imageArr = [
-  "https://images.pexels.com/photos/1036620/pexels-photo-1036620.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/1036621/pexels-photo-1036621.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/1586973/pexels-photo-1586973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-];
-
-function LoadImages() {
-  const [img, setImg] = setImg(false);
-
-  return (
-    <div className="flex">
-      {imageArr.map((image, index) => (
-        <div key={index}>
-          <img
-            src={image}
-            className="w-[50px] h-screen object-cover object-[40%_45%]"
-          />
-        </div>
-      ))}
-    </div>
-  );
-}
-
-function Slides() {
-  return (
-    <div>
-      <LoadImages />
-    </div>
-  );
-}
-
-export default Slides;
