184a4081eb77ea0d4df4a17979e6cbd1d488cb29
msnkr <mikyle.designs@gmail.com>
2024-11-01 12:09:24 +0200

    .

--- product-list-with-cart/src/components/YourCart.js
+++ product-list-with-cart/src/components/YourCart.js
@@ -2,8 +2,6 @@ import data from "../data.json";
 import { useState, useEffect } from "react";
 
 function YourCart({ index, quantity }) {
-  const [currentFood, setCurrentFood] = useState("");
-
   return (
     <div>
       <div className="mt-16 px-8 pb-20">
@@ -12,6 +10,7 @@ function YourCart({ index, quantity }) {
             <h1 className="text-4xl pt-4 font-bold text-rose-700">
               Your Cart (8)
             </h1>
+            <p>{index}</p>
           </div>
         </div>
       </div>
