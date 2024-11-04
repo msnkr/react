eb53851ba4e8739bbf7e45e36f02b06591b880ab
msnkr <mikyle.designs@gmail.com>
2024-11-04 11:45:43 +0200

    .

--- social-media-dashboard/src/App.js
+++ social-media-dashboard/src/App.js
@@ -1,16 +1,22 @@
 import BigCard from "./components/BigCard";
+import iconFacebook from "./images/icon-facebook.svg";
+import iconIncrease from "./images/icon-up.svg";
 
 import { useState } from "react";
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
   return (
-    <div className={`p-8 ${darkMode ? "bg-black" : "bg-white"}`}>
+    <div
+      className={`p-8 ${
+        darkMode ? "bg-black text-white" : "bg-white text-black"
+      }`}
+    >
       <div className="section-1 my-2">
         <div>
           <h1 className="text-3xl">Social Media Dashboard</h1>
         </div>
-        <div className="text-xl">Total Followers: 0000</div>
+        <div className="text-xl">Total Followers: 23, 004</div>
       </div>
       <div>
         <div className="bg-black w-full h-[1px]"></div>
@@ -20,7 +26,14 @@ export default function App() {
         <button>toggle</button>
       </div>
       <div className="section-2">
-        <BigCard />
+        <BigCard
+          lineColor={"bg-yellow-500"}
+          icon={iconFacebook}
+          name={"@nathanf"}
+          followerCount={1987}
+          followers={"followers"}
+          arrowIcon={iconIncrease}
+        />
       </div>
       <div className="section-3"></div>
       <div className="section-4"></div>
