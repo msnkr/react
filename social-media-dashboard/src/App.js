f528e65a8b2194921d09fbdb518be1d1ecaf21b7
msnkr <mikyle.designs@gmail.com>
2024-11-05 09:59:06 +0200

    .

--- social-media-dashboard/src/App.js
+++ social-media-dashboard/src/App.js
@@ -1,5 +1,6 @@
 import BigCard from "./components/BigCard";
 import SmallCard from "./components/SmallCard";
+
 import iconFacebook from "./images/icon-facebook.svg";
 import iconInstagram from "./images/icon-instagram.svg";
 import iconTwitter from "./images/icon-twitter.svg";
@@ -22,45 +23,49 @@ export default function App() {
         darkMode
           ? "bg-very-dark-blue text-white"
           : "bg-white text-dark-grayish-blued"
-      }`}
+      } xl:px-[200px]`}
     >
-      <div className="section-1 my-2">
-        <div>
-          <h1
-            className={`${
-              darkMode ? "text-white" : "text-black"
-            } duration-1000 text-3xl font-bold`}
-          >
-            Social Media Dashboard
-          </h1>
+      <div className="xl:flex justify-between items-center">
+        <div className="section-1 my-2 xl:space-y-4">
+          <div>
+            <h1
+              className={`${
+                darkMode ? "text-white" : "text-black"
+              } duration-1000 text-3xl font-bold`}
+            >
+              Social Media Dashboard
+            </h1>
+          </div>
+          <div className="text-xl font-semibold">Total Followers: 23, 004</div>
         </div>
-        <div className="text-xl font-semibold">Total Followers: 23, 004</div>
-      </div>
-      <div>
-        <div
-          className={`${
-            darkMode ? "bg-desaturated-blue" : "bg-very-pale-blue-top-patter"
-          } w-full h-[3px]`}
-        ></div>
-      </div>
-      <div className="flex justify-between mt-2">
-        <h1 className="font-bold text-lg">Dark Mode</h1>
-        <button
-          onClick={toggleDarkMode}
-          className={`${
-            darkMode
-              ? "bg-gradient-to-r from-toggle-start to-toggle-end"
-              : "bg-dark-grayish-blued"
-          } duration-1000 w-[70px] h-[32px] rounded-full`}
-        >
+        <div>
           <div
             className={`${
-              darkMode ? "bg-very-dark-blue-top-pattern" : "bg-white ml-10"
-            } w-[25px] h-[25px] rounded-full ml-1 duration-1000`}
+              darkMode ? "bg-desaturated-blue" : "bg-very-pale-blue-top-patter"
+            } w-full h-[3px] xl:hidden`}
           ></div>
-        </button>
+        </div>
+        <div className="xl-container">
+          <div className="flex justify-between mt-2 xl:block xl:space-y-4">
+            <h1 className="font-bold text-lg">Dark Mode</h1>
+            <button
+              onClick={toggleDarkMode}
+              className={`${
+                darkMode
+                  ? "bg-gradient-to-r from-toggle-start to-toggle-end"
+                  : "bg-dark-grayish-blued"
+              } duration-1000 w-[70px] h-[32px] rounded-full`}
+            >
+              <div
+                className={`${
+                  darkMode ? "bg-very-dark-blue-top-pattern" : "bg-white ml-10"
+                } w-[25px] h-[25px] rounded-full ml-1 duration-1000`}
+              ></div>
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="section-2 mt-8 space-y-8">
+      <div className="section-2 mt-8 space-y-8 xl:grid grid-cols-4 gap-8 xl:space-y-0">
         <BigCard
           isDarkMode={darkMode}
           lineColor={"bg-facebook"}
@@ -110,7 +115,7 @@ export default function App() {
         <p className="text-3xl text-white">Overview - Today</p>
       </div>
       <div className="section-4">
-        <div className="space-y-4">
+        <div className="space-y-4 xl:grid grid-cols-4 gap-8 xl:space-y-0">
           <SmallCard
             isDarkMode={darkMode}
             heading={"Page Views"}
