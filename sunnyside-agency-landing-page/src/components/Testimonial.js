0f156d987bea13621a127a00fb5888b594ef22fa
msnkr <mikyle.designs@gmail.com>
2024-11-06 16:09:01 +0200

    .

--- sunnyside-agency-landing-page/src/components/Testimonial.js
+++ sunnyside-agency-landing-page/src/components/Testimonial.js
@@ -1,17 +1,17 @@
 function Testimonial({ image, wording, name, position }) {
   return (
-    <div className="text-center">
+    <div className="text-center mt-12 space-y-6">
       <div>
-        <img src={image} />
+        <img className="rounded-full mx-auto" src={image} />
       </div>
       <div>
-        <p>{wording}</p>
+        <p className="text-xl font-barlow font-semibold">{wording}</p>
       </div>
       <div>
-        <p>{name}</p>
+        <p className="text-2xl font-bold">{name}</p>
       </div>
       <div>
-        <p>{position}</p>
+        <p className="text-red-100">{position}</p>
       </div>
     </div>
   );
