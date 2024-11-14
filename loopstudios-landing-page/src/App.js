db5d1d04354fd8f0217e7bcaad8b24448cddb243
msnkr <mikyle.designs@gmail.com>
2024-11-14 10:48:37 +0200

    .

--- loopstudios-landing-page/src/App.js
+++ loopstudios-landing-page/src/App.js
@@ -7,8 +7,13 @@ function App() {
       <div>
         <Navbar />
       </div>
-      <div className="main-image">
+      <div className="main-image relative -z-50">
         <img src={mainImage} />
+        <div className="main-wording absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <p className="text-white text-6xl border-2 px-12 py-8 font-alata uppercase font-extralight">
+            Immerseive Experiences That Deliver
+          </p>
+        </div>
       </div>
     </div>
   );
