66efee67c6a8a36af6ffd2dd661722c278793169
msnkr <mikyle.designs@gmail.com>
2024-11-26 10:13:44 +0200

    .

--- loopstudios-landing-page/src/App.js
+++ loopstudios-landing-page/src/App.js
@@ -13,8 +13,10 @@ function App() {
         </div>
       </div>
       <div>
-        <div>
-          <p>Hello, World!</p>
+        <div className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <p className="text-5xl uppercase border-2 border-white px-12 py-8 leading-10 font-extralight">
+            Immersive experiences that deliver
+          </p>
         </div>
       </div>
     </div>
--- loopstudios-landing-page/src/components/Navbar.js
+++ loopstudios-landing-page/src/components/Navbar.js
@@ -3,12 +3,12 @@ import burger from "../images/icon-hamburger.svg";
 
 function Navbar() {
   return (
-    <div className="p-12 absolute flex items-center">
+    <div className="p-12 absolute flex items-center w-full justify-between">
       <div>
         <img src={logo} />
       </div>
       <div>
-        <img src={burger} />
+        <img className="cursor-pointer" src={burger} />
       </div>
     </div>
   );
