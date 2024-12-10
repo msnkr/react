8878de3cdad39092148fdb0fce6049094a2a4a8b
msnkr <mikyle.designs@gmail.com>
2024-12-10 08:17:04 +0200

    .

--- test-app/src/components/CancelButton.js
+++
@@ -1,9 +0,0 @@
-import cancelButton from "../images/cancel.svg";
-
-export default function CancelButton({ item, id }) {
-  return (
-    <button>
-      <img onClick={() => item(id)} src={cancelButton} className="w-6 h-6" />
-    </button>
-  );
-}
