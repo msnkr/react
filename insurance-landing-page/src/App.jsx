cfdfd3833c4ba73418469ccc4b752dc6371d42f9
msnkr <mikyle.designs@gmail.com>
2025-02-04 15:55:49 +0200

    initialize project with Vite and React setup, add Tailwind CSS configuration, and create initial application structure

---
+++ insurance-landing-page/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div>
+      <img src='https://images.pexels.com/photos/3389817/pexels-photo-3389817.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='w-full h-dvh' />
+      <div className="bg-black w-full h-dvh absolute top-0 opacity-35"></div>
+    </div>
+  )
+}
+export default App
