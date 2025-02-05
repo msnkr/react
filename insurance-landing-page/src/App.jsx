52760d5f0181eeb19ca7aea722b9c45b15a8c359
msnkr <mikyle.designs@gmail.com>
2025-02-05 12:52:45 +0200

    add custom fonts, update layout with logo and introductory section, and define new color and font families in Tailwind CSS

--- insurance-landing-page/src/App.jsx
+++ insurance-landing-page/src/App.jsx
@@ -1,18 +1,35 @@
-import "./App.css"
-import navIcon from "./assets/icon-hamburger.svg"
+import navIcon from "./assets/icon-hamburger.svg";
+import logo from "./assets/logo.svg";
+import imageIntro from "./assets/image-intro-mobile.jpg";
 
 function App() {
   return (
-    <div>
-      <div className="navbar-container flex justify-between p-12">
+    <div className="font-fontKarla">
+      <div className="navbar-container flex justify-between px-12 py-8 items-center">
         <div>
-          <p>INSURE</p>
+          <img src={logo} alt="logo-icon" />
         </div>
         <div>
           <img src={navIcon} alt="hamburger-icon" />
         </div>
       </div>
+      <div className="into">
+        <div>
+          <img src={imageIntro} className="w-full" alt="image-intro" />
+        </div>
+      </div>
+      <div className="bg-dark-violet w-full text-white">
+        <p className="text-5xlfont-semibold font-fontDM">
+          Humanizing your insurance
+        </p>
+        <p className="text-white">
+          Get your life insurance coverage easier and faster. We blend our
+          expertise and technology to help you find the plan that's right for
+          you. Ensure you and your loved ones are protected.{" "}
+        </p>
+        <button className="">VIEW PLANS</button>
+      </div>
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- insurance-landing-page/tailwind.config.js
+++ insurance-landing-page/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-violet": "hsl(256, 26%, 20%)",
+      },
+      fontFamily: {
+        fontDM: "DM Serif Display",
+        fontKarla: "Karla",
+      },
+    },
   },
   plugins: [],
 };
