94decee722237f6932ab529b8367ec8ad7f0fff9
msnkr <mikyle.designs@gmail.com>
2025-02-05 07:49:14 +0200

    add custom fonts, update App component layout, and include SVG icons for navigation

--- insurance-landing-page/src/App.jsx
+++ insurance-landing-page/src/App.jsx
@@ -1,10 +1,9 @@
-
+import "./App.css"
 
 function App() {
   return (
     <div>
-      <img src='https://images.pexels.com/photos/3389817/pexels-photo-3389817.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='w-full h-dvh' />
-      <div className="bg-black w-full h-dvh absolute top-0 opacity-35"></div>
+      <p>Hello, World!</p>
     </div>
   )
 }
