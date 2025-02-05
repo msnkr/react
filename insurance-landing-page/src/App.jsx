1ad44ff7bb944826f2ed2abef9204d97e2f722c7
msnkr <mikyle.designs@gmail.com>
2025-02-05 12:57:53 +0200

    add background patterns to App component and restructure layout for improved design

--- insurance-landing-page/src/App.jsx
+++ insurance-landing-page/src/App.jsx
@@ -1,6 +1,8 @@
 import navIcon from "./assets/icon-hamburger.svg";
 import logo from "./assets/logo.svg";
 import imageIntro from "./assets/image-intro-mobile.jpg";
+import introLeftMobile from "./assets/bg-pattern-intro-left-mobile.svg";
+import introRightMobile from "./assets/bg-pattern-intro-right-mobile.svg";
 
 function App() {
   return (
@@ -18,16 +20,24 @@ function App() {
           <img src={imageIntro} className="w-full" alt="image-intro" />
         </div>
       </div>
-      <div className="bg-dark-violet w-full text-white">
-        <p className="text-5xlfont-semibold font-fontDM">
-          Humanizing your insurance
-        </p>
-        <p className="text-white">
-          Get your life insurance coverage easier and faster. We blend our
-          expertise and technology to help you find the plan that's right for
-          you. Ensure you and your loved ones are protected.{" "}
-        </p>
-        <button className="">VIEW PLANS</button>
+      <div>
+        <div className="bg-dark-violet w-full text-white">
+          <div>
+            <img src={introLeftMobile} alt="pattern" />
+          </div>
+          <p className="text-5xlfont-semibold font-fontDM">
+            Humanizing your insurance
+          </p>
+          <p className="text-white">
+            Get your life insurance coverage easier and faster. We blend our
+            expertise and technology to help you find the plan that's right for
+            you. Ensure you and your loved ones are protected.{" "}
+          </p>
+          <button className="">VIEW PLANS</button>
+          <div>
+            <img src={introRightMobile} alt="pattern" />
+          </div>
+        </div>
       </div>
     </div>
   );
