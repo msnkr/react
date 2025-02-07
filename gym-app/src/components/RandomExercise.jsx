152bca07e061dcce541aeb6aa0aab3b02cf89e98
msnkr <mikyle.designs@gmail.com>
2025-02-07 14:25:07 +0200

    update RandomExercise component to display five random exercises with details

--- gym-app/src/components/RandomExercise.jsx
+++ gym-app/src/components/RandomExercise.jsx
@@ -1,9 +1,24 @@
 import exercises from "../assets/exercise.json";
 
 export default function RandomExercise() {
+  let exerciseArr = [];
+
+  for (let index = 0; index < 5; index++) {
+    let randomIndex = Math.floor(Math.random() * exercises.length);
+    exerciseArr.push(exercises[randomIndex]);
+  }
+
   return (
     <div>
-      <p>Hello, World!</p>
+      {exerciseArr.map((element, index) => {
+        return (
+          <div key={index}>
+            <p>{element.name}</p>
+            <p>{element.primaryMuscles}</p>
+            <p>{element.level}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
