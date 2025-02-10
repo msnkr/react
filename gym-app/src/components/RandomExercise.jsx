7b0b43e3cccbce611d96c660aedf2dd92257ead4
msnkr <mikyle.designs@gmail.com>
2025-02-10 09:52:37 +0200

    remove unused exercise images from assets

--- gym-app/src/components/RandomExercise.jsx
+++ gym-app/src/components/RandomExercise.jsx
@@ -4,14 +4,31 @@ import { use, useState } from "react";
 export default function RandomExercise() {
   const [exerciseArr, setExerciseArr] = useState([]);
 
-  for (let index = 0; index < 5; index++) {
+  const addRandomExercise = () => {
     let randomIndex = Math.floor(Math.random() * exercises.length);
+    setExerciseArr((prevExercise) => {
+      return [...prevExercise, exercises[randomIndex]];
+    });
+  };
+  if (exerciseArr.length < 10) {
+    addRandomExercise();
   }
-
-  console.log(exerciseArr);
   return (
     <div>
-      <div></div>
+      <div>
+        {exerciseArr.map((exercise, index) => {
+          return (
+            <div key={index} className="mt-12">
+              <img
+                src={`../public/exercise-images/${exercise.images[0]}`}
+                alt={exercise.name}
+                className="w-full h-[200px] object-cover"
+              />
+              <p className="text-2xl">{exercise.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
