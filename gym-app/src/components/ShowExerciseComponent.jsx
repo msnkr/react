1cbf4a7beb76a27d728790f217c40e4336147cab
msnkr <mikyle.designs@gmail.com>
2025-02-12 11:44:01 +0200

    feat: enhance ShowExerciseComponent to display detailed exercise information including category, equipment, and instructions

--- gym-app/src/components/ShowExerciseComponent.jsx
+++ gym-app/src/components/ShowExerciseComponent.jsx
@@ -5,13 +5,25 @@ const ShowExerciseComponent = ({ clickedName }) => {
   return (
     <div>
       <div>
-        <p>{clickedName}</p>
+        <p className="text-center my-4 text-2xl">{clickedName}</p>
       </div>
       <div>
         {data.map((exercise, index) => {
           return (
-            <div>
+            <div className="flex flex-col justify-center items-center text-center">
               <img src={`../public/exercise-images/${exercise.images[0]}`} />
+              <div className="capitalize space-y-2 my-8 px-20">
+                <p>category: {exercise.category}</p>
+                <p>equipment: {exercise.equipment}</p>
+                <p>force: {exercise.force}</p>
+                <p>Level: {exercise.level}</p>
+                <p>mechanic: {exercise.mechanic}</p>
+                <p>Primary muscle: {exercise.primaryMuscles}</p>
+                <p>Secondary muscle: {exercise.secondaryMuscles}</p>
+                <div>
+                  <p>{exercise.instructions}</p>
+                </div>
+              </div>
               <img src={`../public/exercise-images/${exercise.images[1]}`} />
             </div>
           );
