8e225ab91e409dcd400ea6f7fb5e505dbb82c472
msnkr <mikyle.designs@gmail.com>
2025-02-12 12:22:15 +0200

    feat: enhance ShowExerciseComponent to improve layout and display detailed exercise instructions and images

--- gym-app/src/components/ShowExerciseComponent.jsx
+++ gym-app/src/components/ShowExerciseComponent.jsx
@@ -5,26 +5,42 @@ const ShowExerciseComponent = ({ clickedName }) => {
   return (
     <div>
       <div>
-        <p className="text-center my-4 text-2xl">{clickedName}</p>
+        <p className="text-center my-4 text-2xl px-12">{clickedName}</p>
       </div>
       <div>
         {data.map((exercise, index) => {
           return (
             <div className="flex flex-col justify-center items-center text-center">
-              <img src={`../public/exercise-images/${exercise.images[0]}`} />
-              <div className="capitalize space-y-2 my-8 px-20">
-                <p>category: {exercise.category}</p>
-                <p>equipment: {exercise.equipment}</p>
-                <p>force: {exercise.force}</p>
-                <p>Level: {exercise.level}</p>
-                <p>mechanic: {exercise.mechanic}</p>
-                <p>Primary muscle: {exercise.primaryMuscles}</p>
-                <p>Secondary muscle: {exercise.secondaryMuscles}</p>
-                <div>
+              <div className="space-y-2 my-8 px-12">
+                <p className="capitalize">category: {exercise.category}</p>
+                <p className="capitalize">equipment: {exercise.equipment}</p>
+                <p className="capitalize">force: {exercise.force}</p>
+                <p className="capitalize">Level: {exercise.level}</p>
+                <p className="capitalize">mechanic: {exercise.mechanic}</p>
+                <p className="capitalize">
+                  Primary muscle: {exercise.primaryMuscles}
+                </p>
+                <p className="capitalize">
+                  Secondary muscle:
+                  {exercise.secondaryMuscles.map((secondary, index) => {
+                    return <div>{exercise.secondaryMuscles[index]}</div>;
+                  })}
+                </p>
+                <div className="mt-8">
+                  <p>Instructions</p>
                   <p>{exercise.instructions}</p>
                 </div>
               </div>
-              <img src={`../public/exercise-images/${exercise.images[1]}`} />
+              <div className="space-y-2">
+                <img
+                  src={`../public/exercise-images/${exercise.images[0]}`}
+                  className="rounded-lg"
+                />
+                <img
+                  src={`../public/exercise-images/${exercise.images[1]}`}
+                  className="rounded-lg"
+                />
+              </div>
             </div>
           );
         })}
