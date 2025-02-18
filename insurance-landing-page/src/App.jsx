204fa584fc4a793a940f71aac72eb2e4021295e2
msnkr <mikyle.designs@gmail.com>
2025-02-18 12:05:26 +0200

    feat: add new section highlighting the application's snappy process with relevant imagery

--- insurance-landing-page/src/App.jsx
+++ insurance-landing-page/src/App.jsx
@@ -4,6 +4,8 @@ import mobileIntro from "./assets/image-intro-mobile.jpg";
 import mobilePatternLeft from "./assets/bg-pattern-intro-left-mobile.svg";
 import mobilePatternRight from "./assets/bg-pattern-intro-right-mobile.svg";
 
+import snappyProcess from "./assets/icon-snappy-process.svg";
+
 function App() {
   return (
     <div className="font-fontKarla">
@@ -43,6 +45,21 @@ function App() {
             <button className="border px-3 py-2 mt-4 uppercase">
               View Plans
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="different-section">
+        <div className="text-center mt-40">
+          <p className="text-5xl font-fontDM">We're different</p>
+        </div>
+        <div className="card flex flex-col justify-center items-center text-center">
+          <img src={snappyProcess} alt="snappy" />
+          <div>
+            <p> Snappy Process</p>
+            <p>
+              Our application process can be completed in minutes, not hours.
+              Don’t get stuck filling in tedious forms.
+            </p>
           </div>
         </div>
       </div>
