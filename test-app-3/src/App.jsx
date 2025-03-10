ae26026c05f79d1be12726678b3a2b04feb30ebd
msnkr <mikyle.designs@gmail.com>
2025-03-10 15:10:51 +0200

    refactor: remove ClickedItems, ClickedPage, and NotFoundPage components; simplify routing in App component

--- test-app-3/src/App.jsx
+++ test-app-3/src/App.jsx
@@ -1,15 +1,8 @@
-import { Link } from "react-router-dom";
-
 const App = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-dvh space-y-8 font-mono">
-      <p className="text-4xl font-semibold">Hello, World!</p>
-      <Link to={"/clicked"}>
-        <button className="px-4 py-2 border border-black rounded-lg scale-95 hover:scale-100 duration-300 ">
-          Click Me!
-        </button>
-      </Link>
-    </div>
+    <>
+      <p>Hello, World!</p>
+    </>
   );
 };
 
--- test-app-3/src/main.jsx
+++ test-app-3/src/main.jsx
@@ -3,19 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ClickedPage from "../pages/ClickedPage";
-import NotFoundPage from "../pages/NotFoundPage.jsx";
-import ClickedItems from "../pages/ClickedItems.jsx";
-
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/clicked", element: <ClickedPage /> },
-  { path: "*", element: <NotFoundPage /> },
-  { path: "/clicked/:id", element: <ClickedItems /> },
-]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
