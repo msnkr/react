7d3a8a9fa5b4fdd2ed6c22eadd11b3e2b6298a17
msnkr <mikyle.designs@gmail.com>
2025-03-10 10:50:50 +0200

    feat: implement routing with React Router and update App component structure

--- test-app-3/src/App.jsx
+++ test-app-3/src/App.jsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-dvh space-y-8">
-      <p className="text-4xl font-mono font-semibold">Hello, World!</p>
-      <button className="px-4 py-2 border-2 border-black rounded-lg scale-95 hover:scale-100 duration-300 ">
+    <div className="flex flex-col justify-center items-center h-dvh space-y-8 font-mono">
+      <p className="text-4xl font-semibold">Hello, World!</p>
+      <button className="px-4 py-2 border border-black rounded-lg scale-95 hover:scale-100 duration-300 ">
         Click Me!
       </button>
     </div>
