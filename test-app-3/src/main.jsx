5847a2e98caef16aa9bdfa4407ac669868a8e55f
msnkr <mikyle.designs@gmail.com>
2025-03-10 12:12:44 +0200

    feat: update routing to include NotFoundPage for handling 404 errors

--- test-app-3/src/main.jsx
+++ test-app-3/src/main.jsx
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ClickedPage from "../pages/ClickedPage";
+import NotFoundPage from "../pages/NotFoundPage.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/clicked", element: <ClickedPage /> },
-  // { path: "*", element: <NotFoundPage /> },
+  { path: "*", element: <NotFoundPage /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
