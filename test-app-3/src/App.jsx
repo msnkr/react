090de292ade2e06c38595fdf39233e15c74362af
msnkr <mikyle.designs@gmail.com>
2025-03-10 10:46:13 +0200

    feat: initialize project with Vite, React, and Tailwind CSS setup

---
+++ test-app-3/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-dvh space-y-8">
+      <p className="text-4xl font-mono font-semibold">Hello, World!</p>
+      <button className="px-4 py-2 border-2 border-black rounded-lg scale-95 hover:scale-100 duration-300 ">
+        Click Me!
+      </button>
+    </div>
+  );
+};
+
+export default App;
