7ca2117dfd74047477d7ea91db2f5539b12ce03a
msnkr <mikyle.designs@gmail.com>
2025-03-12 09:12:30 +0200

    feat: wrap exercise items in Link component for navigation

--- gym-app/src/components/RandomExerciseComponent.jsx
+++ gym-app/src/components/RandomExerciseComponent.jsx
@@ -1,24 +1,28 @@
+import { Link } from "react-router-dom";
+
 const RandomExercise = ({ arr }) => {
   return (
     <div className="w-96 flex flex-col justify-center items-center mx-auto my-4">
       {arr.map((exercise, index) => {
         return (
-          <div
-            key={index}
-            className="my-2 border p-4 rounded-lg shadow-lg w-80"
-          >
-            <img
-              src={`../../public/exercise-images/${exercise.images[1]}`}
-              alt="exercise-image"
-              className="rounded-lg w-full h-[200px] object-cover"
-            />
-            <div className="space-y-2 capitalize">
-              <p className="mt-2 text-xl font-semibold">{exercise.name}</p>
-              <p>category: {exercise.category}</p>
-              <p>level: {exercise.level}</p>
-              <p>Primary Muscle: {exercise.primaryMuscles}</p>
+          <Link>
+            <div
+              key={index}
+              className="my-2 border p-4 rounded-lg shadow-lg w-80"
+            >
+              <img
+                src={`../../public/exercise-images/${exercise.images[1]}`}
+                alt="exercise-image"
+                className="rounded-lg w-full h-[200px] object-cover"
+              />
+              <div className="space-y-2 capitalize">
+                <p className="mt-2 text-xl font-semibold">{exercise.name}</p>
+                <p>category: {exercise.category}</p>
+                <p>level: {exercise.level}</p>
+                <p>Primary Muscle: {exercise.primaryMuscles}</p>
+              </div>
             </div>
-          </div>
+          </Link>
         );
       })}
     </div>
