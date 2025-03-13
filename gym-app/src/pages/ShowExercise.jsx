0d2478cc35f06d7324dd52b939cab4c48676797c
msnkr <mikyle.designs@gmail.com>
2025-03-13 15:51:57 +0200

    feat: enhance ShowExercise component with category and equipment icons for improved display

--- gym-app/src/pages/ShowExercise.jsx
+++ gym-app/src/pages/ShowExercise.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import weightIcon from "../../public/icons/weight.png";
 import levelIcon from "../../public/icons/level-up.png";
 import forceIcon from "../../public/icons/pull.png";
+import categoryIcon from "../../public/icons/category.png";
+import equipmentIcon from "../../public/icons/weight.png";
 
 const ShowExercise = () => {
   const { id } = useParams();
@@ -33,15 +35,22 @@ const ShowExercise = () => {
       </div>
       <div className="capitalize mt-12 space-y-4">
         <div className="flex items-center space-x-2">
-          <img src={levelIcon} alt="level-icon" className="w-8" />
+          <img src={levelIcon} alt="level-icon" className="w-6" />
           <p>Level: {workout.level}</p>
         </div>
         <div className="flex items-center space-x-2">
-          <img src={forceIcon} alt="level-icon" className="w-8" />
+          <img src={forceIcon} alt="force-icon" className="w-6" />
           <p>Force: {workout.force}</p>
         </div>
         <p>Mechanic: {workout.mechanic}</p>
-        <p>Equipment: {workout.equipment}</p>
+        <div className="flex items-center space-x-2">
+          <img src={categoryIcon} alt="category-icon" className="w-6" />
+          <p>Category: {workout.category}</p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <img src={equipmentIcon} alt="category-icon" className="w-6" />
+          <p>Equipment: {workout.equipment}</p>
+        </div>
         <p>Primary Muscle: {workout.primaryMuscles}</p>
         <p>Secondary Muscle: </p>
         <div className="grid grid-cols-4">
