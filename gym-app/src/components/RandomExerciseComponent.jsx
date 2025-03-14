f17a0e5f9b81316f2b80fd2a1d9a438cf127b797
msnkr <mikyle.designs@gmail.com>
2025-03-14 10:08:19 +0200

    feat: restructure RandomExerciseComponent layout for better readability and improved styling

--- gym-app/src/components/RandomExerciseComponent.jsx
+++ gym-app/src/components/RandomExerciseComponent.jsx
@@ -5,23 +5,36 @@ const RandomExercise = ({ arr }) => {
     <div>
       {arr.map((exercise) => {
         return (
-          <Link key={exercise.id} to={`${exercise.id - 1}`}>
-            <div className="my-8">
-              <div className="my-2">
-                <img
-                  src={`../../public/exercise-images/${exercise.images[1]}`}
-                  alt="exercise-image"
-                  className="w-full h-[300px] object-cover"
-                />
-              </div>
-              <div className="space-y-2 capitalize px-4">
-                <p>{exercise.name}</p>
-                <p>{exercise.level}</p>
-                <p>{exercise.equipment}</p>
-                <p>{exercise.primaryMuscles}</p>
-              </div>
+          <div key={exercise.id} className="my-8">
+            <div className="my-2">
+              <img
+                src={`../../public/exercise-images/${exercise.images[1]}`}
+                alt="exercise-image"
+                className="w-full h-[300px] object-cover"
+              />
             </div>
-          </Link>
+            <div className="space-y-2 capitalize px-4">
+              <Link to={`${exercise.id - 1}`}>
+                <p className="font-semibold my-2">{exercise.name}</p>
+              </Link>
+              <p>
+                <span className="text-gray-400">Level: </span>
+                {exercise.level}
+              </p>
+              <p>
+                <span className="text-gray-400">Equipment: </span>
+                {exercise.equipment}
+              </p>
+              <p>
+                <span className="text-gray-400">Primary Muscle: </span>
+                {exercise.primaryMuscles}
+              </p>
+              <p>
+                <span className="text-gray-400">Mechanic: </span>
+                {exercise.force}
+              </p>
+            </div>
+          </div>
         );
       })}
     </div>
