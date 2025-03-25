b2056b5c777c264db0bc3ad24945845fe4c8f15d
msnkr <mikyle.designs@gmail.com>
2025-03-25 11:02:00 +0200

    .

--- gym-app/src/pages/Category.jsx
+++ gym-app/src/pages/Category.jsx
@@ -1,12 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import RandomExercise from "../components/RandomExerciseComponent";
 
 import data from "../assets/data.json";
 
 const Category = () => {
+  const [categoryItem, setCategoryItem] = useState([]);
   const { id } = useParams();
 
-  return <p>Hello</p>;
+  const filteredCategory = data.filter((exercise) => {
+    return exercise.level === id;
+  });
+
+  return (
+    <div>
+      <RandomExercise arr={filteredCategory} />
+    </div>
+  );
 };
 
 export default Category;
