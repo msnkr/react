40bed88b536698efaa534b778597df1b66d11ac8
msnkr <mikyle.designs@gmail.com>
2025-03-26 10:36:06 +0200

    feat: initialize React project with Vite and Tailwind CSS setup

---
+++ printing-company/printing_company/src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p className="text-5xl font-bold">Hello, World!</p>
+    </div>
+  );
+}
+
+export default App;
