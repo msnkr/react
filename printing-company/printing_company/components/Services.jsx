f9b00c34b20e14db5b8a0cd88042540270241f6b
msnkr <mikyle.designs@gmail.com>
2025-03-27 12:50:33 +0200

    fix: update Services component to display text instead of image

--- printing-company/printing_company/components/Services.jsx
+++ printing-company/printing_company/components/Services.jsx
@@ -1,7 +1,7 @@
 const Services = ({ image, text, description }) => {
   return (
     <div className="w-full h-[200px]">
-      <img src={image} className="w-full h-[200px] object-cover" />
+      <p>{text}</p>
     </div>
   );
 };
