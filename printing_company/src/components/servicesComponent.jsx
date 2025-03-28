3d17e038782f45dc12c1921746da66b3e4836bb5
msnkr <mikyle.designs@gmail.com>
2025-03-28 11:52:42 +0200

    feat: update image styling in services component for better visual presentation

--- printing_company/src/components/servicesComponent.jsx
+++ printing_company/src/components/servicesComponent.jsx
@@ -1,7 +1,7 @@
 const ServicesComponent = ({ text, image }) => {
   return (
     <div className="relative rounded-lg border">
-      <img src={image} className="w-full h-[300px] rounded-lg" />
+      <img src={image} className="w-full h-[300px] rounded-lg object-cover" />
       <p className="p-4 cursor-pointer font-semibold underline underline-offset-8 primary-font">
         {text}
       </p>
