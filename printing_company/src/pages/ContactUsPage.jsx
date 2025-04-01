e86b6a97925f806b44d0fc5c53f6fec3393f3951
msnkr <mikyle.designs@gmail.com>
2025-04-01 15:02:10 +0200

    feat: enhance Contact Us page with additional information and a quote request form

--- printing_company/src/pages/ContactUsPage.jsx
+++ printing_company/src/pages/ContactUsPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import NavBar from "../components/NavBar";
 import NavBarLinks from "../components/NavBarLinks";
+import FormComponent from "../components/FormComponent";
 
 const ContactUsPage = () => {
   const [showNav, setShowNav] = useState(false);
@@ -15,6 +16,132 @@ const ContactUsPage = () => {
     <div>
       <div className={`${showNav ? "hidden invisible" : "block visible"}`}>
         <NavBar clicked={handleNav} />
+      </div>
+      <div className="mb-20">
+        <div className="px-12">
+          <p>
+            At Tycole Media & Print, we’re here to assist you with all your
+            printing needs. Whether you have a question, need a quote, or want
+            to discuss a custom project, our team is ready to help.
+          </p>
+        </div>
+        <div className="px-12">
+          <p className="text-2xl primary-font">Get in touch</p>
+          <ul>
+            <li>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                  />
+                </svg>
+                Address:
+              </div>
+              16 Something Road, Somewhereburg, Johannesburg
+              <br />
+              Gauteng 2000
+            </li>
+            <li>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+                Telephone:
+              </div>
+              071 234 5678
+            </li>
+            <li>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
+                  />
+                </svg>
+                Fax:
+              </div>
+              011 345 6789
+            </li>
+            <li>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z"
+                  />
+                </svg>
+                Email:
+              </div>
+              fake@fakeemail.com
+            </li>
+            <li>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                  />
+                </svg>
+                Business Hours:
+              </div>
+              Monday - Friday : 8:00 - 17:00 <br /> Saturday : 9:00 - 14:00{" "}
+              <br /> Sundays & Public Holidays: Closed
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-2xl primary-font px-12">Request A Quote</p>
+          <FormComponent />
+        </div>
       </div>
       <div className={`${showNav ? "visible block" : "hidden invisible"}`}>
         <NavBarLinks clicked={handleNav} />
