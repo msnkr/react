a0f29110222377503395bc61b7b7beecef64a852
msnkr <mikyle.designs@gmail.com>
2025-04-02 10:58:02 +0200

    feat: refactor CarouselComponent to import images from a separate module

--- printing_company/src/components/CarouselComponent.jsx
+++ printing_company/src/components/CarouselComponent.jsx
@@ -1,39 +1,4 @@
-const calendarOne = new URL("../assets/images/calendar-1.jpg", import.meta.url);
-const calendarTwo = new URL("../assets/images/calendar-2.jpg", import.meta.url);
-const calendarThree = new URL(
-  "../assets/images/calendar-3.jpg",
-  import.meta.url
-);
-const businessCardsOne = new URL(
-  "../assets/images/business-cards-1.jpg",
-  import.meta.url
-);
-const businessCardsTwo = new URL(
-  "../assets/images/business-cards-2.jpg",
-  import.meta.url
-);
-const businessCardsThree = new URL(
-  "../assets/images/business-cards-3.jpg",
-  import.meta.url
-);
-const brochureOne = new URL(
-  "../assets/images/broschure-1.jpg",
-  import.meta.url
-);
-const flyerOne = new URL("../assets/images/flyer-1.jpg", import.meta.url);
-const flyerTwo = new URL("../assets/images/flyer-2.jpg", import.meta.url);
-
-const images = [
-  calendarOne,
-  calendarTwo,
-  calendarThree,
-  businessCardsOne,
-  businessCardsTwo,
-  businessCardsThree,
-  brochureOne,
-  flyerOne,
-  flyerTwo,
-];
+import images from "./images";
 
 const handleNext = () => {};
 
