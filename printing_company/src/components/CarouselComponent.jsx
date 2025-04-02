f6f26f51c69b774131f2ea257f07f2a57c78617c
msnkr <mikyle.designs@gmail.com>
2025-04-02 09:57:02 +0200

    feat: add CarouselComponent to ServicePage for enhanced image display

---
+++ printing_company/src/components/CarouselComponent.jsx
@@ -0,0 +1,43 @@
+const CarouselComponent = ({ arr, number }) => {
+  return (
+    <div className="carousel mt-12 relative">
+      <div>
+        <img className="w-full h-[350px]" />
+        <div className="absolute top-1/2 left-8 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
+        </div>
+        <div className="absolute top-1/2 right-8 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarouselComponent;
--- printing_company/src/pages/ServicePage.jsx
+++ printing_company/src/pages/ServicePage.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import NavBar from "../components/NavBar";
 import NavBarLinks from "../components/NavBarLinks";
 import FooterComponent from "../components/FooterComponent";
+import CarouselComponent from "../components/CarouselComponent";
+
+import calendarOne from "../assets/images/calendar-1.jpg";
 
 const ServicePage = () => {
   const [showNav, setShowNav] = useState(false);
@@ -201,42 +204,8 @@ const ServicePage = () => {
             </ul>
           </div>
         </div>
-        <div className="carousel mt-12 relative">
-          <div>
-            <img className="w-full h-[350px]" />
-            <div className="absolute top-1/2 left-8 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15.75 19.5 8.25 12l7.5-7.5"
-                />
-              </svg>
-            </div>
-            <div className="absolute top-1/2 right-8 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                />
-              </svg>
-            </div>
-          </div>
+        <div>
+          <CarouselComponent />
         </div>
         <div className="mt-8 p-8">
           Let’s Print Your Vision! Explore our{" "}
